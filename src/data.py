e2d6d9707ad92c9936ba68cd25a1694c8a9d4aee
Mattias van Dael <mvandael@octinion.com>
2022-02-06 19:20:45 +0100

    create dataloader

--- src/data.py
+++ src/data.py
@@ -1,8 +1,9 @@
+from calendar import c
 import torch
 import unicodedata
 import string
 import random
-
+from torch.utils.data import Dataset
 
 def unicodeToAscii(s):
     return "".join(
@@ -18,6 +19,7 @@ def character_onehot(character, character_lookup):
 
     return onehot
 
+
 def clean_line(line):
     line = line.strip()
     # line = line.lower()
@@ -25,21 +27,6 @@ def clean_line(line):
 
     return line
 
-def get_input_tensor(line, character_lookup):
-    onehot_matrix = [character_onehot(c, character_lookup) for c in line]
-    # onehot_matrix.append(character_onehot("EOL"))
-    tensor = torch.tensor(onehot_matrix)
-    tensor = torch.unsqueeze(tensor, 0)
-
-    return tensor
-
-def get_target_tensor(line, character_lookup):
-    letter_indexes = [character_lookup[char] for i, char in enumerate(line) if i != 0]
-    letter_indexes.append(character_lookup['EOL'])
-    tensor =  torch.LongTensor(letter_indexes)
-    tensor = torch.unsqueeze(tensor, 0)
-
-    return tensor
 
 def decode_tensor(tensor, characters, topk=1):
     try:
@@ -51,11 +38,74 @@ def decode_tensor(tensor, characters, topk=1):
 
     return next_char
 
+
 def get_entry(text, text_length, segment_length):
-    start = random.randint(0, text_length-segment_length)
-    batch = text[start:start+segment_length]
+    start = random.randint(0, text_length - segment_length)
+    batch = text[start : start + segment_length]
 
     return batch
 
+
 def get_batch(batch_size, text, text_length, segment_length):
     return [get_entry(text, text_length, segment_length) for _ in range(batch_size)]
+
+
+
+class TextDataset(Dataset):
+    def __init__(self, text_file, segment_length, character_lookup=None):
+        with open(text_file, "r") as f:
+            self.text = "".join(f.readlines())
+
+        if character_lookup is None:
+            characters = list(set(self.text))
+            self.character_lookup = {character: i for i, character in enumerate(characters)}
+        else:
+            character_lookup = character_lookup
+
+        self.segment_length = segment_length
+
+    def __len__(self):
+        return len(self.text)
+
+    def __getitem__(self, idx):
+        start = random.randint(0, len(self) - self.segment_length)
+        line = self.text[start : start + self.segment_length]
+
+        input = self.get_input_tensor(line)
+        output = self.get_input_tensor(line)
+
+        return input, output
+
+    def get_input_tensor(self, line):
+        onehot_matrix = [character_onehot(c, self.character_lookup) for c in line]
+        # onehot_matrix.append(character_onehot("EOL"))
+        tensor = torch.tensor(onehot_matrix)
+        # tensor = torch.unsqueeze(tensor, 0)
+
+        return tensor
+
+
+    def get_target_tensor(self, line):
+        letter_indexes = [self.character_lookup[char] for i, char in enumerate(line) if i != 0]
+        letter_indexes.append(self.character_lookup["EOL"])
+        tensor = torch.LongTensor(letter_indexes)
+        # tensor = torch.unsqueeze(tensor, 0)
+
+        return tensor
+
+
+if __name__ == '__main__':
+    ds = TextDataset('data/texts/shakespeare.txt', 300)
+
+    from torch.utils.data import DataLoader
+
+    dataloader = DataLoader(ds, batch_size=4, shuffle=False)
+
+    for i in range(1):
+        batch = next(iter(dataloader))
+
+        print(batch[0].shape)
+        print(batch[1].shape)
+
+
+
